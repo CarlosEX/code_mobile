b46ed4bcf57979bc0c18d4946099aaecc3a5e00b
Carlos Antonio <carlos22martinssilva@gmail.com>
2022-02-23 14:02:58 -0300

    commit initial

---
+++ script.js
@@ -0,0 +1,19 @@
+function changeTheme(){
+	if(document.getElementById('bt-theme').innerHTML==='Dark Theme'){
+		document.body.style.background='#555';
+		document.body.style.color='white';
+		document.getElementById('bt-theme').innerHTML='Light Theme'
+	}
+	else{
+		document.body.style.background='white';
+		document.body.style.color='#555';
+		document.getElementById('bt-theme').innerHTML='Dark Theme';
+	}
+}
+
+function changeYoutube(){
+  if(document.getElementById('bt-youtube')){
+    window.open("https://google.com");  
+  }
+}
+
